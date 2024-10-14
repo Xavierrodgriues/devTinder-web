24179c59a5ad8ad8de539129a452550fe6650d0b
Xavierrodgriues <xavierrodgriues123@gmail.com>
2024-10-14 21:56:00 +0530

    Edit profile done

---
+++ src/componenets/EditProfile.jsx
@@ -0,0 +1,150 @@
+import { useState } from "react";
+import UserCard from "./UserCard";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+
+const EditProfile = ({ user }) => {
+  const [firstName, setFirstname] = useState(user.firstName);
+  const [lastName, setLastname] = useState(user.lastName);
+  const [age, setAge] = useState(user.age);
+  const [gender, setGender] = useState(user.gender);
+  const [about, setAbout] = useState(user.about);
+  const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
+  const [skills, setSkills] = useState(user.skills || []); // Initialize skills from user data
+  const [error, setError] = useState("");
+  const [showToast, SetToast] = useState(false);
+  const dispatch = useDispatch();
+  const saveProfile = async () => {
+    setError("");
+    try {
+      const res = await axios.patch(
+        BASE_URL + "/profile/edit",
+        {
+          firstName,
+          lastName,
+          age,
+          gender,
+          about,
+          photoUrl,
+          skills,
+        },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res?.data?.data));
+      SetToast(true);
+      setTimeout(()=>{
+        SetToast(false)
+      },3000)
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
+  console.log(user);
+
+  return (
+    <>
+      <div className="flex justify-evenly my-10 flex-wrap">
+        <div className="flex justify-center my-10 items-center">
+          <div className="card bg-base-300 w-full max-w-4xl shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title justify-center">
+                <u>Edit Profile</u>
+              </h2>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
+                <label className="form-control w-full">
+                  <div className="label">
+                    <span className="label-text">FirstName:</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={firstName}
+                    placeholder="Type here"
+                    className="input input-bordered w-full"
+                    onChange={(e) => setFirstname(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full">
+                  <div className="label">
+                    <span className="label-text">LastName:</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={lastName}
+                    placeholder="Type here"
+                    className="input input-bordered w-full"
+                    onChange={(e) => setLastname(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full">
+                  <div className="label">
+                    <span className="label-text">Age:</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={age}
+                    placeholder="Type here"
+                    className="input input-bordered w-full"
+                    onChange={(e) => setAge(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full">
+                  <div className="label">
+                    <span className="label-text">Gender:</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={gender}
+                    placeholder="Type here"
+                    className="input input-bordered w-full"
+                    onChange={(e) => setGender(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full">
+                  <div className="label">
+                    <span className="label-text">Photo:</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={photoUrl}
+                    placeholder="Type here"
+                    className="input input-bordered w-full"
+                    onChange={(e) => setPhotoUrl(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full">
+                  <div className="label">
+                    <span className="label-text">About:</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={about}
+                    placeholder="Type here"
+                    className="input input-bordered w-full"
+                    onChange={(e) => setAbout(e.target.value)}
+                  />
+                </label>
+              </div>
+              <p className="text-red-500">{error}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary" onClick={saveProfile}>
+                  Save Profile
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <UserCard user={{ firstName, lastName, about, age, photoUrl }} />
+      </div>
+      {showToast && <div className="toast toast-top toast-center">
+        <div className="alert alert-success">
+          <span>Profile saved successfully</span>
+        </div>
+      </div>}
+    </>
+  );
+};
+
+export default EditProfile;
